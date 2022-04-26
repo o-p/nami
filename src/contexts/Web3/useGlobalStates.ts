1affa74de5061a3cf065fc36234c1a6847e11cf6
Chris Chu <funnymaster@gmail.com>
2022-04-27 03:32:27 +0800

    Replace reducer function

--- src/contexts/Web3/useGlobalStates.ts
+++ src/contexts/Web3/useGlobalStates.ts
@@ -14,7 +14,7 @@ import useThunderHub from './useThunderHub'
 import {
   PlayEvent,
   UnboxEvent,
-  useP,
+  usePMT,
   useSlotMachine,
   ERC20 as AbiERC20,
   Multicall2 as AbiMulticall2,
@@ -80,6 +80,63 @@ const initState: AppGlobalState = {
   actions: {},
 }
 
+function GlobalReducer(state: AppGlobalState, action: any) {
+  switch (action.type) {
+    case 'UNBOX_EVENTS':
+      return {
+        ...state,
+        events: {
+          ...state.events,
+          unbox: action.payload,
+        },
+      }
+    case 'SET_PROVIDER':
+      return {
+        ...state,
+        network: {
+          ...state.network,
+          isReadOnly: action.payload.isReadOnly,
+          defaultProvider: action.payload.defaultProvider,
+          providers: {
+            ...state.network.providers,
+            ...action.payload.providers,
+          },
+        },
+      }
+    case 'NETWORK_ERROR':
+      return {
+        ...state,
+        network: {
+          ...state.network,
+          error: action.payload,
+        },
+      }
+    case 'GAME_STATE':
+      return {
+        ...state,
+        game: {
+          ...state.game,
+          ...action.payload.game,
+        },
+        network: {
+          ...state.network,
+          ...action.payload.network,
+        },
+        balances: {
+          ...state.balances,
+          ...action.payload.balances,
+        },
+      }
+    case 'ERROR':
+      return {
+        ...state,
+        error: action.payload,
+      }
+    default:
+      throw new Error('Invalid action')
+  }
+}
+
 function useClientWallet() {
   const wallet = useWallet({ pollBalanceInterval: 3000 })
   const { isHub, hubData } = useThunderHub()
@@ -116,12 +173,9 @@ export default function useGlobalStates() {
   const [lastAutoRefresh, triggerAutoRefresh] = useState(0)
 
   const wallet = useClientWallet()
-  const [fullState, changeState] = useReducer((full: AppGlobalState, changes: Partial<AppGlobalState>) => ({
-    ...full,
-    ...changes,
-  }), initState)
+  const [fullState, dispatch] = useReducer(GlobalReducer, initState)
 
-  const tokenPMT = useP(fullState.network.defaultProvider) as null | {
+  const tokenPMT = usePMT(fullState.network.defaultProvider) as null | {
     balanceOf: (account: string) => Promise<BigNumber>
     approve: (spender: string, amount: BigNumberish) => Promise<ContractTransaction>
     filters: {
@@ -143,6 +197,9 @@ export default function useGlobalStates() {
       toBlock?: number
     ) => Promise<ContractEvent[]>
   }
+
+  const readonlyPMT = usePMT(fullState.network.providers.readonly)
+  const readonlySlotMachine = useSlotMachine(fullState.network.providers.readonly)
 
   const play = useCallback(async (bet: number) => {
     if (wallet.status !== 'connected') throw new Error('Unable to play game before connecting')
@@ -232,18 +289,18 @@ export default function useGlobalStates() {
   }, [fullState.network.defaultProvider])
 
   const queryUnboxHistory = useCallback(async () => {
-    if (!game || !tokenPMT) throw new Error('Contract disconnected')
+    if (!readonlySlotMachine || !readonlyPMT) throw new Error('Contract disconnected')
 
     const { blockNumber } = fullState.network
 
-    const burnedEvents = tokenPMT.queryFilter(
-      tokenPMT.filters.Transfer(null, constants.AddressZero),
+    const burnedEvents = readonlyPMT.queryFilter(
+      readonlyPMT.filters.Transfer(null, constants.AddressZero),
       blockNumber - 86400 * 2,
       blockNumber
     )
 
-    const successUnboxEvents = game.queryFilter(
-      'Unbox',
+    const successUnboxEvents = readonlySlotMachine.queryFilter(
+      readonlySlotMachine.filters.Unbox(),
       blockNumber - 86400 * 2,
       blockNumber
     )
@@ -267,53 +324,47 @@ export default function useGlobalStates() {
         },
       }), history)
     }).then((history) => {
-      changeState({
-        events: {
-          ...fullState.events,
-          unbox: Object
-            .entries(history)
-            .map(([blockNumber, { from, earned }]) => ({
-              blockNumber: parseInt(blockNumber, 10),
-              from,
-              earned,
-            }))
-            .reverse()
-        }
+      dispatch({
+        type: 'UNBOX_EVENTS',
+        payload: Object
+          .entries(history)
+          .map(([blockNumber, { from, earned }]) => ({
+            blockNumber: parseInt(blockNumber, 10),
+            from,
+            earned,
+          }))
+          .reverse()
       })
     }).catch((e) => { console.error('Query unbox records failure', e) })
-    /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, [fullState.network, game])
+  }, [
+    readonlyPMT,
+    readonlySlotMachine,
+    fullState.game.unboxFee,
+    fullState.network,
+  ])
 
   const setConnection = useCallback(async (method: keyof Connectors | '') => {
     debug('switch connection to: %s', method)
     if (method !== '') {
       try {
         await wallet.connect(method)
-        changeState({
-          network: {
-            ...fullState.network,
-            isReadOnly: false,
-            defaultProvider: method,
-            error: null,
-          },
+        dispatch({
+          type: 'NETWORK_ERROR',
+          payload: null,
         })
       } catch (error) {
         debug('Set connection error: %o', error)
-        changeState({
-          network: {
-            ...fullState.network,
-            isReadOnly: true,
-            defaultProvider: 'readonly',
-            error,
-          }
+        dispatch({
+          type: 'NETWORK_ERROR',
+          payload: error,
         })
       }
     } else {
-      changeState({
-        network: {
-          ...fullState.network,
+      dispatch({
+        type: 'SET_PROVIDER',
+        payload: {
           isReadOnly: true,
-          defaultProvider: 'readonly',
+          defaultProvider: fullState.network.providers.readonly,
         },
       })
     }
@@ -327,15 +378,13 @@ export default function useGlobalStates() {
           params: [configs.network],
         })
         .catch((error: any) => {
-          changeState({
-            network: {
-              ...fullState.network,
-              error,
-            },
+          dispatch({
+            type: 'NETWORK_ERROR',
+            payload: error,
           })
         })
     }
-  }, [fullState.network])
+  }, [])
 
   const approvePMT = useCallback(async () => {
     if (wallet.status !== 'connected') throw new Error('Unable to approve before connecting')
@@ -361,7 +410,6 @@ export default function useGlobalStates() {
     const userAddress = wallet.account ?? ''
     const multicallAddress = fullState.configs.contract.Multicall2
 
-    // if (current.fetching) return Promise.reject('fetching')
     if (
       Date.now() - current.lastUpdate < 1000
       && current.lastUpdateSuccess
@@ -401,7 +449,7 @@ export default function useGlobalStates() {
 
     if (userAddress) {
       requests.push({
-        reference: 'P',
+        reference: 'PMT',
         contractAddress: tokenAddress,
         abi: AbiERC20,
         calls: [
@@ -427,31 +475,29 @@ export default function useGlobalStates() {
 
     return multicall
       .call(requests)
-      .then(({ results: { gameUnboxFee, gameAcculatedPrize, P, TT, chain } }) => {
-        const game = { ...fullState.game }
-        const balanceP = { ...fullState.balances.P }
-        const balanceTT = { ...fullState.balances.TT }
-        const network = { ...fullState.network }
+      .then(({ results: { gameUnboxFee, gameAcculatedPrize, PMT, TT, chain } }) => {
+        const game: any = {}
+        const balances: any = {}
 
-        if (chain.callsReturnContext[0].success) network.blockNumber = ethers.BigNumber.from(chain.callsReturnContext[0].returnValues[0]).toNumber()
         if (gameUnboxFee.callsReturnContext[0].success) game.unboxFee = ethers.BigNumber.from(gameUnboxFee.callsReturnContext[0].returnValues[0])
         if (gameAcculatedPrize.callsReturnContext[0].success) game.acculatedPrize = ethers.BigNumber.from(gameAcculatedPrize.callsReturnContext[0].returnValues[0])
 
-        const [allowance, balanceOfP] = P?.callsReturnContext ?? []
+        const [allowance, balancePMT] = PMT?.callsReturnContext ?? []
         if (allowance?.success) game.dpAllowance = ethers.BigNumber.from(allowance.returnValues[0])
-        if (balanceOfP?.success) Object.assign(balanceP, { amount: ethers.BigNumber.from(balanceOfP.returnValues[0]), display: formatToken(balanceOfP.returnValues[0]) })
+        if (balancePMT?.success) balances.P = { amount: ethers.BigNumber.from(balancePMT.returnValues[0]), display: formatToken(balancePMT.returnValues[0]) }
 
-        const [balanceOfTT] = TT?.callsReturnContext ?? []
-        if (balanceOfTT?.success) Object.assign(balanceTT, { amount: ethers.BigNumber.from(balanceOfTT.returnValues[0]), display: formatToken(balanceOfTT.returnValues[0]) })
+        const [balanceTT] = TT?.callsReturnContext ?? []
+        if (balanceTT?.success) balances.TT = { amount: ethers.BigNumber.from(balanceTT.returnValues[0]), display: formatToken(balanceTT.returnValues[0]) }
 
-        changeState({
-          game,
-          balances: {
-            ...fullState.balances,
-            P: balanceP,
-            TT: balanceTT,
+        dispatch({
+          type: 'GAME_STATE',
+          payload: {
+            game,
+            balances,
+            network: chain.callsReturnContext[0].success
+              ? { blockNumber: ethers.BigNumber.from(chain.callsReturnContext[0].returnValues[0]).toNumber() }
+              : null,
           },
-          network,
         })
       })
       .then(() => {
@@ -473,8 +519,8 @@ export default function useGlobalStates() {
     /* eslint-disable-next-line react-hooks/exhaustive-deps */
   }, [fullState.network.defaultProvider])
 
-  const showError = useCallback((error: Error | string) => changeState({ error }), [])
-  const clearError = useCallback(() => changeState({ error: null }), [])
+  const showError = useCallback((error: Error | string) => dispatch({ type: 'ERROR', payload: error }), [])
+  const clearError = useCallback(() => dispatch({ type: 'ERROR', payload: null }), [])
 
   // on wallet initialized
   useEffect(() => {
@@ -489,23 +535,20 @@ export default function useGlobalStates() {
       const { ethereum } = wallet
       const provider = new ethers.providers.Web3Provider(ethereum as ExternalProvider)
       const signer = provider.getSigner().connectUnchecked()
-      changeState({
-        ...fullState,
-        network: {
-          ...fullState.network,
+      dispatch({
+        type: 'SET_PROVIDER',
+        payload: {
           providers: {
-            ...fullState.network.providers,
             web3: provider,
           },
-          defaultProvider: signer,
           isReadOnly: false,
+          defaultProvider: signer,
         },
       })
     } else if (fullState.network.defaultProvider !== fullState.network.providers.readonly) {
-      changeState({
-        ...fullState,
-        network: {
-          ...fullState.network,
+      dispatch({
+        type: 'SET_PROVIDER',
+        payload: {
           defaultProvider: fullState.network.providers.readonly,
           isReadOnly: true,
         },
